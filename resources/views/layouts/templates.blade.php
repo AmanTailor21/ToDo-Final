d6ff53d94c48cfaf462029b9835ecf4f335f3db6
AmanTailor21 <an.tailor007@gmail.com>
2021-04-01 18:44:12 +0530

    Movie Task

--- resources/views/layouts/templates.blade.php
+++ resources/views/layouts/templates.blade.php
@@ -9,6 +9,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="{{ asset("/js/jquery.js") }}" type="text/javascript"></script>
+    <script src="{{ asset("/js/style.js") }}" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"
+            type="text/javascript"></script>
     <title>ToDo</title>
 </head>
 <body>
@@ -26,19 +31,19 @@
 </div>
 <div class="m-3">
     @if(isset($todos))
-    <form action="/">
-        <input type="submit" class="btn btn-info" value="ADD TASK">
-    </form>
+        <form action="/">
+            <input type="submit" class="btn btn-info" value="ADD TASK">
+        </form>
     @else
         <div class="p-4"></div>
     @endif
 </div>
 <div class="card m-3 shadow-sm" style="height: 700px;">
     <div class="row">
-        <div class="col-lg-6" style="height:700px;overflow-y: scroll;">
+        <div class="col-lg-6" id="subDiv" style="height:700px;overflow-y: scroll;">
 
             @foreach($todo as $task)
-                <div class="row">
+                <div class="row m-3 border-bottom">
                     <div class="col-lg-1 m-3">
                         <input type="checkbox">
                     </div>
@@ -50,7 +55,7 @@
                             <p><strike>{{$task->notes}}</strike></p>
                         @else
                             <b><a href="/edit/{{$task->id}}"
-                                          style="color: #1a202c;">{{$task->title}}</a></b>
+                                  style="color: #1a202c;">{{$task->title}}</a></b>
                             <p>{{$task->notes}}</p>
                         @endif
                     </div>
@@ -58,22 +63,7 @@
                         <i class="fa fa-star-o" aria-hidden="true"></i>
                     </div>
                 </div>
-                <hr>
             @endforeach
-            <div class="row">
-                <div class="col-lg-1 m-3">
-                    <input type="checkbox">
-                </div>
-                <div class="col-lg-8">
-                    <p>Laravel is a web application framework with expressive, elegant syntax. A web framework provides
-                        a structure and starting point for creating your application, allowing you to focus on creating
-                        something amazing while we sweat the details.</p>
-                </div>
-                <div class="col-lg-2 m-3">
-                    <i class="fa fa-star-o" aria-hidden="true"></i>
-                </div>
-            </div>
-            <hr>
         </div>
         <div class="col-lg-6">
 
@@ -84,45 +74,46 @@
                 <div class="container-fluid shadow-sm">
                     <div class="row">
                         <div class="col m-3">
-                            <input type="checkbox" name="mark" {{ (!empty($todos) &&  $todos["mark"] == 1) ? 'checked' : '' }}>
+                            <input type="checkbox"
+                                   name="mark" {{ (!empty($todos) &&  $todos["mark"] == 1) ? 'checked' : '' }}>
                             <label> Mark as Done</label>
                         </div>
                     </div>
                 </div>
                 <div class="m-3">
-                <div class="form-group">
-                    <label>Add Title</label>
-                    <input type="text" name="title" class="form-control" placeholder="Title"
-                           value="{{ (!empty($todos)) ? $todos["title"] : '' }}">
-                    @error('title') <span class="text-danger">{{$message}}</span> @enderror
-                </div>
-                <div class="row">
-                    <div class="form-group col">
-                        <label>Start Date</label>
-                        <input type="date" name="start_date" class="form-control" placeholder="Start Date"
-                               value="{{ (!empty($todos)) ? $todos["start_date"] : '' }}">
-                        @error('start_date') <span class="text-danger">{{$message}}</span> @enderror
+                    <div class="form-group">
+                        <label>Add Title</label>
+                        <input type="text" name="title" class="form-control" placeholder="Title"
+                               value="{{ (!empty($todos)) ? $todos["title"] : '' }}">
+                        @error('title') <span class="text-danger">{{$message}}</span> @enderror
                     </div>
-                    <div class="col">
-                        <label>Due Date</label>
-                        <input type="date" name="due_date" class="form-control" placeholder="Due Date"
-                               value="{{ (!empty($todos)) ? $todos["due_date"] : '' }}">
-                        @error('due_date') <span class="text-danger">{{$message}}</span> @enderror
+                    <div class="row">
+                        <div class="form-group col">
+                            <label>Start Date</label>
+                            <input type="date" name="start_date" class="form-control" placeholder="Start Date"
+                                   value="{{ (!empty($todos)) ? $todos["start_date"] : '' }}">
+                            @error('start_date') <span class="text-danger">{{$message}}</span> @enderror
+                        </div>
+                        <div class="col">
+                            <label>Due Date</label>
+                            <input type="date" name="due_date" class="form-control" placeholder="Due Date"
+                                   value="{{ (!empty($todos)) ? $todos["due_date"] : '' }}">
+                            @error('due_date') <span class="text-danger">{{$message}}</span> @enderror
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label>Add Notes</label>
-                    <textarea placeholder="Notes..." name="notes"
-                              class="form-control">{{ (!empty($todos)) ? $todos["notes"] : '' }}</textarea>
-                    @error('notes') <span class="text-danger">{{$message}}</span> @enderror
-                </div>
-                <div>
-                    @if(isset($todos))
-                        <input type="submit" class="btn btn-info" value="UPDATE TASK">
-                    @else
-                        <input type="submit" class="btn btn-info" value="SAVE">
-                    @endif
-                </div>
+                    <div class="form-group">
+                        <label>Add Notes</label>
+                        <textarea placeholder="Notes..." name="notes"
+                                  class="form-control">{{ (!empty($todos)) ? $todos["notes"] : '' }}</textarea>
+                        @error('notes') <span class="text-danger">{{$message}}</span> @enderror
+                    </div>
+                    <div>
+                        @if(isset($todos))
+                            <input type="submit" class="btn btn-info" value="UPDATE TASK">
+                        @else
+                            <input type="submit" class="btn btn-info" value="SAVE">
+                        @endif
+                    </div>
                 </div>
             </form>
             {{session('msg')}}
@@ -130,16 +121,6 @@
     </div>
 </div>
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+
 </body>
 </html>
